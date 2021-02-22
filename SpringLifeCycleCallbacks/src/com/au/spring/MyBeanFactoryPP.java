7a7b5459e9576fabb3e2ab40f46132cc6798447d
roshnimehata <mehataroshni26@gmail.com>
2021-02-22 23:07:50 +0530

    BeanPostProcessorChnage

--- SpringLifeCycleCallbacks/src/com/au/spring/MyBeanFactoryPP.java
+++ SpringLifeCycleCallbacks/src/com/au/spring/MyBeanFactoryPP.java
@@ -2,15 +2,22 @@ package com.au.spring;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
-public class MyBeanFactoryPP implements BeanFactoryPostProcessor{
+public class MyBeanFactoryPP implements BeanPostProcessor{
 
 	@Override
-	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		// TODO Auto-generated method stub
-		System.out.println("In postProcessbeanFcatory");
-		
-	}
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+        System.out.println("Calling bean post processor before init for bean:: "+beanName + "class name: "+bean.getClass());
+        return bean;
+    }
 
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+                System.out.println("Calling bean post processor after init for bean:: "+beanName + "class name: "+bean.getClass());
+        return bean;
+    }
 }
